2ddc33120e0bf0aee85df3eb840fa88ee705a70c
Jobs <lg295060456@gmail.com>
2024-06-03 19:35:37 +0800

    no message

--- lib/Data/Data.3rd/本地数据存取/StorageHelperDemo.dart
+++ lib/Data/Data.3rd/本地数据存取/StorageHelperDemo.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    storageUsageDemo();
     return MaterialApp(
       title: 'Storage Demo',
       theme: ThemeData(
--- lib/Data/Data.Origin/单例的写法/MySingletonDemo2.dart
+++ lib/Data/Data.Origin/单例的写法/MySingletonDemo2.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/foundation.dart';
-
+// 在 Dart 中，没有内置的方法可以显式销毁一个类的实例，包括单例。
+// 这是因为 Dart 像许多现代编程语言一样，依赖垃圾回收来自动管理内存。
+// 一旦没有对一个对象的引用，垃圾回收器最终会回收该内存。
 class Singleton {
   // 私有构造函数
   Singleton._internal();
-
   // 静态私有实例
+  // 单例对象要被重置为null，要求_instance不能被final修饰，因为不能直接赋值null
   static final Singleton _instance = Singleton._internal();
-
   // 工厂构造函数
   factory Singleton() {
     return _instance;
   }
+  // 销毁单例对象的方法
+  // static void destroyInstance() {
+  //   _instance = null;
+  //   debugPrint("该单例对象已经被销毁");
+  // }
 
-  // 你的业务逻辑方法
-  void someMethod() {
+  // 其他方法
+  void doSomething() {
     debugPrint("这是一个单例方法");
   }
 }
