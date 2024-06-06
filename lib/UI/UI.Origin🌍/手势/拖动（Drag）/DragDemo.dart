f9b3b0bfee77818542191fbcbae1b977845e66e8
Jobs <lg295060456@gmail.com>
2024-06-06 13:57:28 +0800

    no message

--- lib/UI/UI.Origin🌍/手势/拖动（Drag）/DragDemo.dart
+++ lib/UI/UI.Origin🌍/手势/拖动（Drag）/DragDemo.dart
@@ -35,10 +35,3 @@ class DragDemo extends StatelessWidget {
     );
   }
 }
-
-
-  // 类似于Future。GlobalKey这个大类型里面包裹的是FormState这样的小的具体的类型。
-  // 和iOS的key不同（iOS需要手动创建全剧唯一的static字符串），存取铆定这个字符串
-  // Flutter创建这个全局的Key类似，只不过不一定是字符串类型，你也不需要管他具体是什么
-
-  // 创建完了以后，铆定UI。没有设定就是原始的super.key
