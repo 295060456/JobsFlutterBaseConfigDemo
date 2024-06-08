f6e818e49f8ccd93346c3f3800a37318520abe2e
Jobs <lg295060456@gmail.com>
2024-06-08 23:34:35 +0800

    no message

--- lib/UI/UI.3rd🌹/GetX/GetX管理的页面的生命周期/GetX.PageA/controllers/pageA_controllers.dart
+++ lib/UI/UI.3rd🌹/GetX/GetX管理的页面的生命周期/GetX.PageA/controllers/pageA_controllers.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 class PageAControllers extends GetxController {
 
+  final ok = true.obs; 
+
   @override
   void onInit() {
     super.onInit();
@@ -15,5 +17,13 @@ class PageAControllers extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  void reverse (){
+    ok.value = !ok.value;
+  }
+
+  String changeText() {
+    return "qqq";
   }
 }
--- lib/UI/UI.3rd🌹/GetX/GetX管理的页面的生命周期/GetX.PageA/views/pageA_view.dart
+++ lib/UI/UI.3rd🌹/GetX/GetX管理的页面的生命周期/GetX.PageA/views/pageA_view.dart
@@ -6,15 +6,17 @@ import '../controllers/pageA_controllers.dart';
 
 class PageAView extends GetView<PageAControllers> {
   const PageAView({super.key});
-  
   @override
   Widget build(BuildContext context) {
+    /// iOS 的命名风格
+    PageAControllers self = Get.put(PageAControllers());
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             Get.to(() => const PageBView());
+            debugPrint(self.changeText());
           },
           child: const Text('Go to Page B'),
         ),
