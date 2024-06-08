cda13955968dadcd338a7bff9e4805bb92730839
Jobs <lg295060456@gmail.com>
2024-06-08 19:05:15 +0800

    no message

--- lib/UI/UI.3rd🌹/GetX/GetX管理的页面的生命周期/GetX.PageB/controllers/pageB_controllers.dart
+++ lib/UI/UI.3rd🌹/GetX/GetX管理的页面的生命周期/GetX.PageB/controllers/pageB_controllers.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class PageBControllers extends GetxController with WidgetsBindingObserver {
+class PageBControllers extends GetxController {
 
   @override
   void onInit() {
     super.onInit();
-    WidgetsBinding.instance.addObserver(this);
   }
 
   @override
@@ -16,32 +14,6 @@ class PageBControllers extends GetxController with WidgetsBindingObserver {
 
   @override
   void onClose() {
-    WidgetsBinding.instance.removeObserver(this);
     super.onClose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    switch (state) {
-      case AppLifecycleState.inactive:
-        // 应用程序处于非活动状态
-        debugPrint('应用程序处于非活动状态');
-        break;
-      case AppLifecycleState.paused:
-        // 应用程序处于后台
-        debugPrint('应用程序处于后台');
-        break;
-      case AppLifecycleState.resumed:
-        // 应用程序处于前台并可交互
-        debugPrint('应用程序处于前台并可交互');
-        break;
-      case AppLifecycleState.detached:
-        // 应用程序没有关联的 Flutter 引擎
-        debugPrint('应用程序没有关联的 Flutter 引擎');
-        break;
-      case AppLifecycleState.hidden:
-        // TODO: Handle this case.
-    }
-    super.didChangeAppLifecycleState(state);
   }
 }
--- lib/UI/UI.3rd🌹/GetX/GetX管理的页面的生命周期/GetXLifeCycleDemo.dart
+++ lib/UI/UI.3rd🌹/GetX/GetX管理的页面的生命周期/GetXLifeCycleDemo.dart
@@ -7,8 +7,49 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+    @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.inactive:
+        // 应用程序处于非活动状态
+        debugPrint('应用程序处于非活动状态');
+        break;
+      case AppLifecycleState.paused:
+        // 应用程序处于后台
+        debugPrint('应用程序处于后台');
+        break;
+      case AppLifecycleState.resumed:
+        // 应用程序处于前台并可交互
+        debugPrint('应用程序处于前台并可交互');
+        break;
+      case AppLifecycleState.detached:
+        // 应用程序没有关联的 Flutter 引擎
+        debugPrint('应用程序没有关联的 Flutter 引擎');
+        break;
+      case AppLifecycleState.hidden:
+        // TODO: Handle this case.
+    }
+    super.didChangeAppLifecycleState(state);
+  }
 
   @override
   Widget build(BuildContext context) {
