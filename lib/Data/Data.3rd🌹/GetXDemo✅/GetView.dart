74665a75bde1db5f2ff539b281695a7ca9d58dbf
Jobs <lg295060456@gmail.com>
2024-06-12 22:45:08 +0800

    no message

--- lib/Data/Data.3rd🌹/GetXDemo✅/GetView.dart
+++ lib/Data/Data.3rd🌹/GetXDemo✅/GetView.dart
@@ -4,6 +4,9 @@ import 'package:get/get.dart';
 // 你需要利用 GetView 的 controller 属性来管理生命周期，同时确保 WidgetsBindingObserver 逻辑在适当的地方实现。
 // 虽然 GetView 本身是一个 StatelessWidget，但你可以将 WidgetsBindingObserver 的逻辑放到 Controller 中，并且在 GetView 中触发相应的操作。
 // 下面是一个示例，展示如何在 GetView 中实现 WidgetsBindingObserver：
+
+/// 在Dart中,!操作符被称为"非空断言操作符",它告诉编译器该变量绝不会为null。
+/// 但是,如果你在一个可能为null的变量后面使用!操作符,并且这个变量实际上为null,那么就会触发一个NullPointerException异常,导致应用程序崩溃。
 void main() {
   runApp(
     GetMaterialApp(
